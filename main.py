2651ff48660afee6707003c7c202535168ed8d5f
pdev-gd <83932298+pdev-gd@users.noreply.github.com>
2023-10-12 03:22:23 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,77 @@
+from langchain.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import FAISS
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import RetrievalQA
+from langchain.callbacks.base import BaseCallbackHandler
+# from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+import streamlit as st
+import tempfile
+import os
+import dotenv
+import openai
+dotenv_file = dotenv.find_dotenv()
+dotenv.load_dotenv(dotenv_file)
+
+openai.api_key = os.environ["OPENAI_API_KEY"]
+
+#제목
+st.title("ChatPDF")
+st.write("---")
+
+#파일 업로드
+uploaded_file = st.file_uploader("PDF 파일을 올려주세요!",type=['pdf'])
+st.write("---")
+
+def pdf_to_document(uploaded_file):
+    temp_dir = tempfile.TemporaryDirectory()
+    temp_filepath = os.path.join(temp_dir.name, uploaded_file.name)
+    with open(temp_filepath, "wb") as f:
+        f.write(uploaded_file.getvalue())
+    loader = PyPDFLoader(temp_filepath)
+    pages = loader.load_and_split()
+    return pages
+
+#업로드 되면 동작하는 코드
+if uploaded_file is not None:
+    pages = pdf_to_document(uploaded_file)
+
+    #Split
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size = 300,
+        chunk_overlap  = 20,
+        length_function = len,
+        is_separator_regex = False,
+    )
+    texts = text_splitter.split_documents(pages)
+
+    #Embedding
+    embeddings_model = OpenAIEmbeddings(openai_api_key=openai.api_key)
+
+    # load it into FAISS
+    db = FAISS.from_documents(texts, embeddings_model)
+
+    #Stream 받아 줄 Handler 만들기
+    #https://github.com/streamlit/StreamlitLangChain/blob/main/streaming_demo.py
+    class StreamHandler(BaseCallbackHandler):
+        def __init__(self, container, initial_text=""):
+            self.container = container
+            self.text=initial_text
+        def on_llm_new_token(self, token: str, **kwargs) -> None:
+            self.text+=token
+            self.container.markdown(self.text)
+
+    #Question
+    st.header("PDF에게 질문해보세요!!")
+    question = st.text_input('질문을 입력하세요')
+
+    if st.button('질문하기'):
+        with st.spinner('Wait for it...'):
+            chat_box = st.empty()
+            stream_hander = StreamHandler(chat_box)
+            llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0, openai_api_key=openai.api_key , streaming=True, callbacks=[stream_hander])
+            qa_chain = RetrievalQA.from_chain_type(llm,retriever=db.as_retriever())
+            qa_chain({"query": question})
+
+#streamlit run main.py
